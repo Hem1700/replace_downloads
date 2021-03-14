af6427ec97c624349dbadded7805aa6c32e07216
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-03-14 10:38:20 +0530

    First commit

---
+++ mitm.py
@@ -0,0 +1,17 @@
+import netfilterqueue
+import scapy.all as scapy
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())  # This will convert the packet payload to scapy packet
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("HTTP Request")
+            print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print("HTTP Response")
+            print(scapy_packet.show())
+    packet.accept() # This will accept the packet and forward it to the client computer allowing him to go that perticular website
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
