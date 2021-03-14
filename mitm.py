a36af3ea7e69d4776f258c4ac0b1d7b76df48869
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-03-14 16:55:06 +0530

    Analysing HTTP packets

--- mitm.py
+++ mitm.py
@@ -6,7 +6,9 @@ def process_packet(packet):
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
             print("HTTP Request")
-            print(scapy_packet.show())
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                print("[+] exe Request")
+                print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
             print("HTTP Response")
             print(scapy_packet.show())
